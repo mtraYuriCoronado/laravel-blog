734439a8022bd87176afa3eb21b1366f9aaddb86
mtraYuriCoronado <mtra.yuricoronado@gmail.com>
2024-12-02 10:50:38 -0600

    Segundo commit: manejo de rutas básico

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,28 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    //return view('welcome');
+    return "Welcome to the homepage";
 });
+
+Route::get('/posts', function () {
+    //return view('welcome');
+    return "Aqui se enlistan los posts";
+});
+
+// rutas con parámetros
+// Route::get('/posts/{post}', function ($post) {
+//     //return view('welcome');
+//     return "Aqui se muestra el post {$post}";
+// });
+// Route::get('/posts/{category}/{post}', function ($category, $post) {
+//     //return view('welcome');
+//     return "Aqui se muestra el post {$post} de la cateogira {$category}";
+// });
+// Route::get('/posts/{post}/{category?}', function ($post, $category = null) {
+//     //return view('welcome');
+//     if($category){
+//         return "Aqui se muestra el post {$post} de la categoria {$category}";
+//     }
+//     return "Aqui se muestra el post {$post}";
+// });
