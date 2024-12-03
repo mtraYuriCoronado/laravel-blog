b1c65ecba35b336ff1a045ddd8d5406faeb6921c
mtraYuriCoronado <mtra.yuricoronado@gmail.com>
2024-12-03 09:56:42 -0600

    Componentes en laravel

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,12 +1,39 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Laravel 11 Ejemplo</title>
+    <script src="https://cdn.tailwindcss.com"></script>
 </head>
+
 <body>
     <h1>Bienvenido a la página principal</h1>
+
+    <div class="max-w-4xl mx-auto px-4">
+        <x-alert type="danger" class="mb-4">
+            <x-slot name="title">
+                ¡Peligro!
+            </x-slot>
+            Contenido de la alerta
+        </x-alert>
+        <x-alert type="warning" class="mb-4">
+            <x-slot name="title">
+                Cuidado!
+            </x-slot>
+            Contenido de la alerta
+        </x-alert>
+        <x-alert type="error" class="mb-4">
+            <x-slot name="title">
+                Cuidado!
+            </x-slot>
+            Contenido de la alerta
+        </x-alert>
+        <p>Hola mundo</p>
+    </div>
+
 </body>
+
 </html>
